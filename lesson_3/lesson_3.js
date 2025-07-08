184d006dc3f60fa98ed5f050102ad116122838c4
Lach Michal <Misiek@air-rose.home>
2025-07-08 22:35:54 +0200

    update lesson_3.js

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -102,4 +102,23 @@ function draw() {
 // }
 
 
+let radius = 100
 
+function setup() {
+    createCanvas(400, 400);
+    background(60);
+}
+
+function draw() {
+    fill(0);
+    // noStroke
+    fill(255,50);
+    circle(mouseX,mouseY,radius);
+    // size = sin(radius)
+    // radius = radius * size
+}
+
+   
+function mousePressed(){
+    radius = radius/2
+}

9ae29ac691983bc19d04d15226a96577f61bd581
Lach Michal <Misiek@air-rose.home>
2025-07-14 22:06:56 +0200

    do the lesson

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -102,23 +102,3 @@ function draw() {
 // }
 
 
-let radius = 100
-
-function setup() {
-    createCanvas(400, 400);
-    background(60);
-}
-
-function draw() {
-    fill(0);
-    // noStroke
-    fill(255,50);
-    circle(mouseX,mouseY,radius);
-    // size = sin(radius)
-    // radius = radius * size
-}
-
-   
-function mousePressed(){
-    radius = radius/2
-}

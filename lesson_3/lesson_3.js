36a130e17da86f9e6f653b144fe9466118d2ef83
Lach Michal <Misiek@air-rose.home>
2025-07-15 22:46:12 +0200

    add proper spaces

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -1,7 +1,4 @@
-// //zadanie nr 1 
-
-
-
+// Ex 1 
 
 // let radius = 60
 
@@ -11,36 +8,31 @@
 // }
 
 // function draw() {
-   
-    
 //     fill(29,216,219);
-//     radius=60
-//     if (mouseX > 200 ){
+//     radius = 60;
+
+//     if (mouseX > 200 ) {
 //         fill(29,217,79);    
-     
-// } 
+//     } 
+
 //     if (mouseY < 200) {
-//     (radius=20);
+//         (radius=20);
+//     }
 
-    
-// }
-// if (mouseX > 300 && mouseY >300) {
-//     rect(mouseX,mouseY,20,20);
-//     fill(255,0,0);
-// }
-// if (mouseX<20 || mouseY <20){
-//     fill ( 0,0,0)
-// }
+//     if (mouseX > 300 && mouseY >300) {
+//         rect(mouseX,mouseY,20,20);
+//         fill(255,0,0);
+//     }
+
+//     if (mouseX<20 || mouseY <20) {
+//         fill(0,0,0);
+//     }
+
 //     circle(mouseX,mouseY,radius);
-   
-
 // } 
 
 
-
-
-// zadanie nr 2 
-
+// Ex 2 
 
 var x = 0
 var speed = 5
@@ -48,58 +40,46 @@ var speed = 5
 
 function setup (){
     createCanvas (400,400);
-    
-
 }
-function draw() {
-    
- 
 
-    background (4,204,17)
-
+function draw(){
+    background(4,204,17);
     
-    rect(x,20,60,60,20,20,20,20)
-    fill(111,4,204)
+    rect(x,20,60,60,20,20,20,20);
+    fill(111,4,204);
     
-    x=x + speed
-
-    
-    
+    x = x + speed;
 
     if (x==width){
         speed = speed * -1
     }
-    rect(160,160,60,60,20,20,20)
-    fill(204,4,64)
-    if (mouseX>160 && mouseX<200 && mouseY>160 && mouseY<200){ 
-    
-        rect(160,160,60,60,20,20,20)
-        fill(29,216,200)} 
-        if (mouseIsPressed && (mouseX>160 && mouseX<200 && mouseY>160 && mouseY<200)){
-        
-            background(0)
-            fill(255,0,0);
-        }
-        }
 
-        
-        
-       
+    rect(160,160,60,60,20,20,20);
+    fill(204,4,64);
+
+    if (mouseX>160 && mouseX<200 && mouseY>160 && mouseY<200){ 
+        rect(160,160,60,60,20,20,20);
+        fill(29,216,200);
+    }
+
+    if (mouseIsPressed && (mouseX>160 && mouseX<200 && mouseY>160 && mouseY<200)){
+        background(0);
+        fill(255,0,0);
+    }
+
+}
+
 
 
 // to jest oscylator ponizej - zamiennik na ten ruchomy kwadrat , ale mi nie dziala z reszta 
-
 
 // background(4,204,17);
 
 
 // let x = width * sin(frameCount * 0.02) + 50;
-// let y = 250 ;
+// let y = 250;
 
 
 // // line(50, y, x, y);
-// rect(x, y, 50,50,20,20,20,20);
-// fill(127,23,234)}
-
-
-
+// rect(x, y, 50, 50, 20, 20, 20, 20);
+// fill(127, 23, 234)};

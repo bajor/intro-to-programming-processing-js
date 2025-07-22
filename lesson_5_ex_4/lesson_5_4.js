b58c4697bd849cae690ca61b96a51d1aaef268e1
Lach Michal <Misiek@air-rose.home>
2025-07-22 22:44:27 +0200

    example with switching background

--- lesson_5_ex_4/lesson_5_4.js
+++ lesson_5_ex_4/lesson_5_4.js
@@ -1,67 +1,34 @@
 function setup() {
     createCanvas (400,400);
-    time = 2
-    var light= false
-    k = true
-    var on 
-    background(110)
+
+    backgroundSwitch = true;
 }
 
 function draw() {
-    // background (123,14,15);
 
-    rect   (170, 150, 60, 140 );
-    fill   (245,13,0); // fill do czerwonego 
+    console.log(backgroundSwitch);
 
-    rect (180, 240, 40, 40, 20, 20, 20, 20);
+    if (backgroundSwitch == true) {
+        background(255);
+    } else if (backgroundSwitch == false) {
+        background(0);
+    }
+
     fill   (255, 251, 0,); 
+    rect   (170, 150, 60, 140 );
 
-    rect (180, 200, 40, 40, 20, 20, 20, 20);
+    fill   (245,13,0); // fill do czerwonego 
+    rect (180, 240, 40, 40, 20, 20, 20, 20);
+
     fill   (120, 200, 20); 
+    rect (180, 200, 40, 40, 20, 20, 20, 20);
 
-    rect (180, 160, 40, 40, 20, 20, 20, 20);
     fill   (100, 100, 100,); // fill do kwadratu
+    rect (180, 160, 40, 40, 20, 20, 20, 20);
+
 
 }
 
-
-
-    // if mousePressed(){
-    //     background(0);
-    // }
-    // else {background(160)}
-
-
-//             if (light) {
-                
-//                 background(0);}
-            
-//                 else { 
-                
-//                 background (150,10,10);}
-
-
-
-
-
-
-//     mouseIsPressed() {if (on){
-//         light = false ;
-// }
-// else  {light = true;} }
-                
-
-
-
-//         function mouseIsPressed()
-//         if (k){
-//             background(50) ;
-// }
-// else  {background(80);} 
-    
-     
-
-    //  if (mouseX>180 && mouseX<220 && mouseY>240 && mouseY<260){ 
-    //         circle   (200, 220, 40, );
-    //         fill   (1);
-    //     }
+function mousePressed(){
+    backgroundSwitch = !backgroundSwitch;
+}

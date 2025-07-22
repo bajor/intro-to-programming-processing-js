f4081cfcd707b140d6cc673c24cb48002a6af9d9
Lach Michal <Misiek@air-rose.home>
2025-07-22 22:28:04 +0200

    reorganize

--- lesson_5_ex_5/lesson_5_5.js
+++ lesson_5_ex_5/lesson_5_5.js
@@ -1,8 +1,15 @@
 function setup() {
-    createCanvas(400, 400);
-    background(255);
+    createCanvas (500,500)
+    background(0)
+    x = 1
 }
 
 function draw() {
+//     rect (mouseX, 100, 40 ,40 )
+//     fill (100,100,0)
+
+//     if ( mouseIsPressed)
+        
+//     {fill(200,100,0)}}
     background(255);
 }
